dec047900d8d27ea70e6a8ab74dbfd347deadf9c
LisenkoKalina <nekrasova.kalina@yandex.ru>
2020-04-15 14:53:07 +0300

    Добавлено меню

--- js/script.js
+++ js/script.js
@@ -29,3 +29,19 @@ $(document).ready(function(){
         slickPrev: false
     });
   });
+
+function menu(){
+    let menu = document.getElementById('menu');
+    let menu_item = document.getElementById('nav');
+    menu.onclick = function(){
+        menu.classList.toggle('burger-active');
+        if ( menu.classList.contains('burger-active') ){   
+            menu.classList.add('burger-active');
+            menu_item.style.display = "flex";            
+        }else {
+            menu.classList.remove('burger-active');
+            menu_item.style.display = "none";
+        }
+    };
+    
+}
