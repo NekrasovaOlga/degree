1a3fcc5fce1b14c43f79b77aa52a2b2a6528c4e6
LisenkoKalina <nekrasova.kalina@yandex.ru>
2020-04-20 15:49:55 +0300

    Добавлена прокрутка для слайдера

--- js/script.js
+++ js/script.js
@@ -21,18 +21,24 @@ $(function(){
 });
 
 
-$(document).ready(function(){
-    $('.company__slider').slick({
-        infinite: true,
-        slidesToShow: 5,
-        slidesToScroll: 3,
-        slickPrev: false
-    });
-  });
 
+  var slider = new Slider("#ex13", {
+    min: 0,
+    max: 500,
+    ticks_labels: ["100м2", "10 000м2", "250 000м2", "500 000м2"],
+    ticks_snap_bounds: 10,
+    value: 0,
+    focus: true 
+});
+var area = document.getElementById('area');
+
+slider.onclick = function (){
+    area.innerText = slider.getValue();
+}
 function menu(){
     let menu = document.getElementById('menu');
     let menu_item = document.getElementById('nav');
+    
     menu.onclick = function(){
         menu.classList.toggle('burger-active');
         if ( menu.classList.contains('burger-active') ){   
@@ -44,4 +50,43 @@ function menu(){
         }
     };
     
+    
 }
+
+var mySwiper = new Swiper('.swiper-container', {
+    speed: 300,
+    spaceBetween: 100,
+    slidesPerView: 5,
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        clickable: true,
+        draggable: true,
+        dragClass: 'swiper-scrollbar-dra',
+        dragSize: 33
+      },
+      breakpoints: {
+          867:{
+        slidesPerView: 4,
+      }
+      }
+      
+});
+
+function pakt(){
+    let light = document.getElementById('light');
+    let medium = document.getElementById('medium');
+    let full = document.getElementById('full');
+    let cell = document.getElementById('cell');
+    let text_btn = document.getElementById('text_btn');
+    light.onclick = function(){
+        cell.innerText = '4 800 тг';
+        text_btn.style.display = 'block';
+    }
+    medium.onclick = function(){
+        cell.innerText = '7 500 тг';
+    }
+    full.onclick = function(){
+        cell.innerText = '5 500 тг';
+    }
+}
+
